0a0625831ad2046ec561c01365a30311bf51a3fd
Kevaunjh <kevaunjh@gmail.com>
2024-08-22 13:47:55 -0400

    Testing Layout 6

--- src/AccountInfo/BiometricSetups/Fingerprint.js
+++ src/AccountInfo/BiometricSetups/Fingerprint.js
@@ -13,7 +13,7 @@ function BiometricSetup() {
   return (
     <div className="BiometricSetup bg-[#88ca92] h-screen flex items-center justify-center">
       {/* Container for Instructions, Image, and Proceed Button */}
-      <div className="bg-white p-8 xl:p-16 xl:rounded-3xl shadow-2xl flex flex-col items-center w-full xl:max-w-3xl mx-auto relative h-screen">
+      <div className="bg-white p-8 xl:p-16 xl:rounded-3xl shadow-2xl flex flex-col items-center w-full xl:max-w-3xl mx-auto relative h-screen z-10">
         {/* Back Button */}
         <Link to="/MyAccount" className="absolute top-4 left-4 text-lg rounded-full p-2 bg-[#467a4d] text-white">
           &larr; Back
